0dba56e3d72ef19cd797f474a9f7525e59795889
Josoe Santos <jssantosqueiroz@gmail.com>
2023-12-05 22:58:24 -0300

    feat: :white_check_mark: test states 0 to 7

--- controller/tb_script.tcl
+++ controller/tb_script.tcl
@@ -1,5 +1,7 @@
 #
 #vsim -do tb_script.do
+# cd C:/Users/jssan/OneDrive/LabSD/ProjetoFinal/components/controller
+# set ms [examine sim/:tb:uut:imem:mem_array_size]
 
 if {[file exists work]} {
 vdel -lib work -all
@@ -10,9 +12,11 @@ vcom -explicit  -93 "tb_controller.vhd"
 vsim -t 1ns   -lib work tb_controller
 add wave sim:/tb_controller/*
 #do {wave.do}
+add wave sim:/tb_controller/instance_dut/state
 view wave
 view structure
 view signals
 run 200ns
+wave zoom full
 #quit -force
 
